d73bb3ac2d910e6c99c4520e180ed278f455b6ce
Jose Masri <josemasri222@gmail.com>
2019-05-24 13:55:43 -0500

    Agregado ejercicio b y c

---
+++ ejercicio1c.asm
@@ -0,0 +1,26 @@
+.include "m8def.inc"
+.org 0x0000
+rjmp main
+ 
+main:
+ldi r16, low(RAMEND)
+out SPL, r16
+ldi r16, high(RAMEND)
+out SPH, r16
+ 
+ldi r16, 0xFF
+out DDRB, r16
+
+loop:
+sbi PortB, 1
+rcall delay
+cbi PortB, 1
+rcall delay
+rjmp loop
+
+delay:
+ldi r16, 2797
+salto:
+subi r16, 1
+brne salto
+ret
