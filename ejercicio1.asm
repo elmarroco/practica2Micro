1a2759c87f5b1f4d1c289b03a30a7fc67f9ee116
Jose Masri <josemasri222@gmail.com>
2019-05-30 07:37:44 -0500

    Arreglado ejercicio 1

--- ejercicio1.asm
+++ ejercicio1.asm
@@ -1,3 +1,16 @@
+; Frecuencia del reloj 12MHz = 12,000,000 Hz (12,000,000 ciclos por segundo)
+; Necesitamos que la frecuencia del puerto sea  de 1000Hz ()
+; Y se apague durante 500Hz
+; 6,000,000/500 
+; Gastar 12,000 ciclos en el delay
+; for(int i=0; i<n; i++) n veces 125  ldi r12, 0
+;                                     ldi r13, 124
+;                                     subi r12, 1
+;                                     rjmp con_for1
+;                                     
+;   for(int j=0; j<m; j++) m veces 96
+;     a+b
+; 
 .include "m8def.inc"
 .org 0x0000
 rjmp main
@@ -19,8 +32,14 @@ rcall delay
 rjmp loop
 
 delay:
-ldi r16, 25
-salto:
-subi r16, 1
-brne salto
-ret
+ldi r16, 83  ; 1 Clock 125/1.5 = 83
+loop2:
+ldi r17, 64  ; 1 Clock  96/1.5
+
+loop3:
+dec r17      ; 1 Clock
+brne loop2      ; 1/2 Clock
+
+dec r16       ; 1 Clock
+brne loop       ; 1/2 Clock
+ret             ; 4 Clocks
---
+++ ejercicio2.asm
@@ -0,0 +1,47 @@
+
+.org 0x0000
+ 
+rjmp reset
+ 
+.org 0x000E
+rjmp ADC_ISR
+reset:
+ldi r16, low(RAMEND)
+out SPL, r16
+ldi r16, high(RAMEND)
+out SPH, r16
+ 
+ldi r16, 0xFF
+out DDRD, r16
+ 
+ldi r16, 0
+out ADMUX, r16
+ldi r16, 0b11101101
+out ADCSR, r16
+ 
+sei
+ 
+loop:
+rjmp loop
+ 
+ADC_ISR:
+push r16
+in r16, SREG
+push r16
+push r17
+ 
+in r16, ADCL
+in r17, ADCH
+lsr r17
+ror r16
+lsr r17
+ror r16
+
+com r16
+out PortD, r16
+
+pop r17
+pop r16
+out SREG, r16
+pop r16
+ret
